8923973b613190b295ae08161951b608822f6ba0
Lou <marilounacional0717@gmail.com>
2025-06-04 13:06:24 +0800

    add_font

--- quiz_creator.py
+++ quiz_creator.py
@@ -11,4 +11,13 @@ pygame.display.set_caption("Lulu's Quiz Maker")
 bg_image = pygame.image.load("pixel forest.jpg")
 bg_image = pygame.transform.scale(bg_image, (WIDTH, HEIGHT))
 
+# Load pixel font
+font = pygame.font.Font("PressStart2P-Regular.ttf", 18)
+large_font = pygame.font.Font("PressStart2P-Regular.ttf", 24)
+clock = pygame.time.Clock()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (70, 150, 255)
+
 # end
