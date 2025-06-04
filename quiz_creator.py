5503d443ba7b9ea7578bfc6b4b4bee41ae489dd8
Lou <marilounacional0717@gmail.com>
2025-06-04 13:05:20 +0800

    load_background_image

--- quiz_creator.py
+++ quiz_creator.py
@@ -7,5 +7,8 @@ WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Lulu's Quiz Maker")
 
-# main program
+# Load pixel background and scale to screen
+bg_image = pygame.image.load("pixel forest.jpg")
+bg_image = pygame.transform.scale(bg_image, (WIDTH, HEIGHT))
+
 # end
